0e83ff92f806a4d80c450b0d2d71bdf86e0ca8d6
hugescrub <hexi9001@gmail.com>
2022-12-03 21:44:21 +0300

    Payload

---
+++ src/main/java/net/hugescrub/newsservice/payload/MessageResponse.java
@@ -0,0 +1,12 @@
+package net.hugescrub.newsservice.payload;
+
+import lombok.Getter;
+
+@Getter
+public class MessageResponse {
+    private final String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+}
