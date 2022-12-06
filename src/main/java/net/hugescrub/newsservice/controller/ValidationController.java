6789f1e3b2798dd5b3c6e5437a04a6965381c2c2
hugescrub <hexi9001@gmail.com>
2022-12-07 00:15:43 +0300

    Mock classification endpoint

--- src/main/java/net/hugescrub/newsservice/controller/ValidationController.java
+++ src/main/java/net/hugescrub/newsservice/controller/ValidationController.java
@@ -1,28 +1,37 @@
 package net.hugescrub.newsservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import net.hugescrub.newsservice.dto.ArticleDto;
 import net.hugescrub.newsservice.model.Article;
 import net.hugescrub.newsservice.payload.MessageResponse;
 import net.hugescrub.newsservice.repository.ArticleRepository;
 import net.hugescrub.newsservice.service.ArticleService;
+import net.hugescrub.newsservice.service.RequestConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/validator")
 public class ValidationController {
 
     private final ArticleRepository articleRepository;
     private final ArticleService articleService;
+    private final RequestConsumer consumer;
 
     @Autowired
-    public ValidationController(ArticleRepository articleRepository, ArticleService articleService) {
+    public ValidationController(ArticleRepository articleRepository,
+                                ArticleService articleService,
+                                RequestConsumer consumer) {
         this.articleRepository = articleRepository;
         this.articleService = articleService;
+        this.consumer = consumer;
     }
 
     @GetMapping("/get/all")
@@ -52,8 +61,24 @@ public class ValidationController {
             return ResponseEntity.ok()
                     .body(new MessageResponse("The article is fake."));
         } else {
+            Long articleId = articleRepository.findByTitle(articleDto.getTitle()).getId();
+            //consumer.passData(articleId, "/api/classification/classify");
+            consumer.passData(articleId, "/api/validator/classificationMock");
             return ResponseEntity.ok()
                     .body(new MessageResponse("The article is not fake."));
         }
+    }
+
+    @PostMapping("/classificationMock")
+    public ResponseEntity<? extends MessageResponse> respondWithTopic() {
+        log.warn("Classification endpoint was called at: " + LocalTime.now());
+        Map<Integer, String> topics = Map.of(
+                1, "Medicine",
+                2, "Politics",
+                3, "Sports",
+                4, "Science"
+        );
+        return ResponseEntity.ok()
+                .body(new MessageResponse(topics.get((int) (Math.random() * 4))));
     }
 }
