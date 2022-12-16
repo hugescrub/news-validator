8cb7d60875da773b4e489c178b9a60e3e52afbf9
hugescrub <hexi9001@gmail.com>
2022-12-16 14:23:30 +0300

    Get article and change fake requests

--- src/main/java/net/hugescrub/newsservice/service/RequestConsumer.java
+++ src/main/java/net/hugescrub/newsservice/service/RequestConsumer.java
@@ -1,16 +1,19 @@
 package net.hugescrub.newsservice.service;
 
+import net.hugescrub.newsservice.model.Article;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 public class RequestConsumer {
-    //private final String BASE_URL = "http://111.1.1.1:port";
-    private final String BASE_URL = "http://localhost:8080";
+
+    private final String PORTAL_BASE_URL = "http://localhost:8080";
+    private final String CLASSIFICATION_BASE_URL = "http://localhost:8082";
 
     public void passData(final Long articleId, final String uriPath) {
         if (articleId > 0 && uriPath != null) {
-            WebClient client = WebClient.create(BASE_URL);
+            WebClient client = WebClient.create(CLASSIFICATION_BASE_URL);
             client.post()
                     .uri(uriPath)
                     .bodyValue(articleId)
@@ -18,5 +21,33 @@ public class RequestConsumer {
                     .bodyToMono(String.class)
                     .subscribe(System.out::println);
         }
+    }
+
+    public Mono<Article> retrieveData(final Long id, final String uriPath) {
+        if (id > 0 && uriPath != null) {
+            WebClient client = WebClient.create(PORTAL_BASE_URL);
+            return client.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path(uriPath)
+                            .build(id))
+                    .retrieve()
+                    .bodyToMono(Article.class);
+        } else
+            throw new IllegalArgumentException("Article id < 1 or wrong uri provided.");
+    }
+
+    public void changeFake(final Long articleId, final String uriPath, final boolean isFake) {
+        if (articleId > 0 && uriPath != null) {
+            WebClient client = WebClient.create(PORTAL_BASE_URL);
+            client.patch()
+                    .uri(uriBuilder -> uriBuilder
+                            .path(uriPath)
+                            .queryParam("isFake", isFake)
+                            .build(articleId))
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .subscribe(System.out::println);
+        } else
+            throw new IllegalArgumentException("Article id < 1 or wrong uri provided.");
     }
 }
