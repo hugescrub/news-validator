635fa16834a26e670525bf3887e9c12fbde924a2
hugescrub <hexi9001@gmail.com>
2022-12-18 01:18:11 +0300

    Get cookies and pass them

--- src/main/java/net/hugescrub/newsservice/service/ArticleService.java
+++ src/main/java/net/hugescrub/newsservice/service/ArticleService.java
@@ -7,6 +7,7 @@ import net.hugescrub.newsservice.repository.SourceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
 import java.util.Objects;
 import java.util.regex.Pattern;
@@ -30,15 +31,19 @@ public class ArticleService {
 
     // e.g. if article is fake return true, if not return false
     @Transactional
-    public boolean validateArticle(Long articleId) {
+    public boolean validateArticle(Long articleId, HttpServletRequest request) {
+        String authCookie = request.getHeader(RequestConsumer.AUTH_COOKIE);
+
         Article article = Objects.requireNonNull(
-                requestConsumer.retrieveData(articleId, "/portal/news/{id}").block()
+                requestConsumer.retrieveData(articleId, "/portal/news/{id}", authCookie)
+                        .block()
         );
 
         boolean isFake = article.getBody() != null
                 && Pattern.compile(".*gazeta\\.ru.*kommersant\\.ru.*lenta\\.ru.*interfax\\.ru")
                 .matcher(article.getBody()).find();
-        requestConsumer.changeFake(articleId, isFake, "/portal/news/{articleId}");
+
+        requestConsumer.changeFake(articleId, isFake, "/portal/news/{articleId}", authCookie);
         if(!articleRepository.existsByTitle(article.getTitle())) {
             article.setFake(isFake);
             articleRepository.save(article);
